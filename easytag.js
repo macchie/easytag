b5567d815a5fbbe2086d9f00d6af8ef0d31eff39
Andrea Macchieraldo <a.macchieraldo@gmail.com>
2023-04-05 17:34:56 +0200

    update name

--- easytag.js
+++ easytag.js
@@ -5,7 +5,7 @@ const execSync = require('child_process').execSync;
 const semver = require('semver');
 const chalk = require('chalk');
 
-class EasyVER {
+class EasyTAG {
 
   static _formats = {
     '_master': `{{version}}`,
@@ -25,11 +25,11 @@ class EasyVER {
       if (!_pgkJson.scripts) {
         _pgkJson.scripts = {};
       }
-      _pgkJson.scripts.preversion = 'npx easyver';
+      _pgkJson.scripts.preversion = 'npx easytag';
       fs.writeFileSync('package.json', JSON.stringify(_pgkJson, null, 2));
-      console.log(`${chalk.green('SUCCESS:')} EasyVER initialized successfully.`);
+      console.log(`${chalk.green('SUCCESS:')} EasyTAG initialized successfully.`);
     } catch (error) {
-      console.log(`${chalk.red('ERROR:')} Failed to initialize EasyVER.`);
+      console.log(`${chalk.red('ERROR:')} Failed to initialize EasyTAG.`);
       console.log(`\n${chalk.yellow(error.message)}\n`)
       process.exit(1);
     }
@@ -38,14 +38,14 @@ class EasyVER {
   // prepare 
   static prepare() {
     try {
-      EasyVER._pgkJson = JSON.parse(fs.readFileSync('package.json'));
-      EasyVER._gitClean = execSync('git status --porcelain').toString().trim().length === 0;
-      EasyVER._branchName = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
-      EasyVER._oldVersion = process.env.npm_old_version;
-      EasyVER._newVersion = process.env.npm_new_version;
-      EasyVER._action = semver.diff(EasyVER._newVersion, EasyVER._oldVersion);
+      EasyTAG._pgkJson = JSON.parse(fs.readFileSync('package.json'));
+      EasyTAG._gitClean = execSync('git status --porcelain').toString().trim().length === 0;
+      EasyTAG._branchName = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+      EasyTAG._oldVersion = process.env.npm_old_version;
+      EasyTAG._newVersion = process.env.npm_new_version;
+      EasyTAG._action = semver.diff(EasyTAG._newVersion, EasyTAG._oldVersion);
     } catch (error) {
-      console.log(`${chalk.red('ERROR:')} Failed to prepare EasyVER.`);
+      console.log(`${chalk.red('ERROR:')} Failed to prepare EasyTAG.`);
       console.log(`\n${chalk.yellow(error.message)}\n`)
       process.exit(1);
     }
@@ -53,11 +53,11 @@ class EasyVER {
 
   // get tag name based on format
   static getTag(_version) {
-    const _isMaster = ['master', 'main'].includes(EasyVER._branchName);
-    const _tagFormat = _isMaster ? EasyVER._formats._master : EasyVER._formats._branch;
+    const _isMaster = ['master', 'main'].includes(EasyTAG._branchName);
+    const _tagFormat = _isMaster ? EasyTAG._formats._master : EasyTAG._formats._branch;
 
     return _tagFormat
-      .replace('{{branchName}}', EasyVER._branchName)
+      .replace('{{branchName}}', EasyTAG._branchName)
       .replace('{{version}}', `v${_version}`);
   }
 
@@ -72,20 +72,20 @@ class EasyVER {
 
   // verify if all conditions are met
   static verify() {
-    if (!EasyVER._gitClean) {
+    if (!EasyTAG._gitClean) {
       console.log(`${chalk.red('ERROR:')} Current GIT working directory is not clean.`);
       process.exit(1);
     }
 
-    EasyVER._branchName = EasyVER.sanitizeBranchName(EasyVER._branchName);
+    EasyTAG._branchName = EasyTAG.sanitizeBranchName(EasyTAG._branchName);
 
-    if (!EasyVER._branchName || EasyVER._branchName.indexOf('/') > -1) {
+    if (!EasyTAG._branchName || EasyTAG._branchName.indexOf('/') > -1) {
       console.log(`${chalk.red('ERROR:')} Branch Name not found or invalid.`);
       process.exit(1);
     }
 
-    if (!['patch', 'minor', 'major'].includes(EasyVER._action)) {
-      console.log(`${chalk.red('ERROR:')} Invalid Action '${EasyVER._action}'.`);
+    if (!['patch', 'minor', 'major'].includes(EasyTAG._action)) {
+      console.log(`${chalk.red('ERROR:')} Invalid Action '${EasyTAG._action}'.`);
       process.exit(1);
     }
   }
@@ -93,42 +93,42 @@ class EasyVER {
   // execute
   static execute() {
     if (process.argv[2] === '--init') {
-      EasyVER.init();
+      EasyTAG.init();
       process.exit(0);
     }
 
-    EasyVER.prepare() && EasyVER.verify();
+    EasyTAG.prepare() && EasyTAG.verify();
 
-    console.log(`${chalk.cyan('INFO:')} Current version is '${chalk.yellow(EasyVER.getTag(EasyVER._oldVersion))}'...`);
-    console.log(`${chalk.cyan('INFO:')} Releasing '${EasyVER._action}' version '${chalk.yellow(EasyVER.getTag(EasyVER._newVersion))}'...`);
+    console.log(`${chalk.cyan('INFO:')} Current version is '${chalk.yellow(EasyTAG.getTag(EasyTAG._oldVersion))}'...`);
+    console.log(`${chalk.cyan('INFO:')} Releasing '${EasyTAG._action}' version '${chalk.yellow(EasyTAG.getTag(EasyTAG._newVersion))}'...`);
     
     try {
       // update package.json
-      EasyVER._pgkJson.version = EasyVER._newVersion;
-      fs.writeFileSync('package.json', JSON.stringify(EasyVER._pgkJson, null, 2));
+      EasyTAG._pgkJson.version = EasyTAG._newVersion;
+      fs.writeFileSync('package.json', JSON.stringify(EasyTAG._pgkJson, null, 2));
 
       // commit and tag
       execSync(`git add package*.json`);
-      execSync(`git commit -m "${EasyVER.getTag(EasyVER._newVersion)}"`);
-      execSync(`git tag ${EasyVER.getTag(EasyVER._newVersion)}`);
+      execSync(`git commit -m "${EasyTAG.getTag(EasyTAG._newVersion)}"`);
+      execSync(`git tag ${EasyTAG.getTag(EasyTAG._newVersion)}`);
     } catch (error) {
-      console.log(`${chalk.red('ERROR:')} Failed to create '${EasyVER._action}' version '${chalk.yellow(EasyVER.getTag(EasyVER._newVersion))}'!`)
+      console.log(`${chalk.red('ERROR:')} Failed to create '${EasyTAG._action}' version '${chalk.yellow(EasyTAG.getTag(EasyTAG._newVersion))}'!`)
       console.log(`\n${chalk.yellow(error.message)}\n`)
       process.exit(201);
     }
 
-    console.log(`${chalk.green('SUCCESS:')} Created Tag for '${EasyVER._action}' version '${chalk.yellow(EasyVER.getTag(EasyVER._newVersion))}'.`);
+    console.log(`${chalk.green('SUCCESS:')} Created Tag for '${EasyTAG._action}' version '${chalk.yellow(EasyTAG.getTag(EasyTAG._newVersion))}'.`);
 
     try {
       // push
       execSync(`git push && git push --tags`, {stdio: []});
     } catch (error) {
-      console.log(`${chalk.red('ERROR:')} Failed to Push '${EasyVER._action}' version '${chalk.yellow(EasyVER.getTag(EasyVER._newVersion))}'!`)
+      console.log(`${chalk.red('ERROR:')} Failed to Push '${EasyTAG._action}' version '${chalk.yellow(EasyTAG.getTag(EasyTAG._newVersion))}'!`)
       console.log(`\n${chalk.yellow(error.message)}\n`)
       process.exit(202);
     }
 
-    console.log(`${chalk.green('SUCCESS:')} Released '${EasyVER._action}' version '${chalk.yellow(EasyVER.getTag(EasyVER._newVersion))}'!`);
+    console.log(`${chalk.green('SUCCESS:')} Released '${EasyTAG._action}' version '${chalk.yellow(EasyTAG.getTag(EasyTAG._newVersion))}'!`);
     console.log(`${chalk.green('SUCCESS: Ignore the following error messages! ;)')}\n`);
 
     process.exit(200);
@@ -136,7 +136,7 @@ class EasyVER {
 }
 
 try {
-  EasyVER.execute();
+  EasyTAG.execute();
 } catch (error) {
   process.exit(203);  
 }
